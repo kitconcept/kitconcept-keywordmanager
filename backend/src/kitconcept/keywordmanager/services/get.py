532b9b96b4a86d7ac2133a42110c42397d9096d5
jnptk <110389276+jnptk@users.noreply.github.com>
2026-04-13 16:09:32 +0200

    make endpoint traverseable

--- backend/src/kitconcept/keywordmanager/services/get.py
+++ backend/src/kitconcept/keywordmanager/services/get.py
@@ -2,19 +2,41 @@ from kitconcept.keywordmanager.interfaces import IKeywordManager
 from plone.restapi.batching import HypermediaBatch
 from plone.restapi.services import Service
 from zope.component import getUtility
+from zope.interface import implementer
+from zope.publisher.interfaces import IPublishTraverse
 
 
+@implementer(IPublishTraverse)
 class KeywordsGet(Service):
+    """/@keywords Endpoint."""
+
+    def __init__(self, context, request):
+        super().__init__(context, request)
+        self.context = context
+        self.request = request
+        self.params = []
+
+    def publishTraverse(self, request, name):
+        # Consume any path segments after /@keywords as parameters
+        self.params.append(name)
+        return self
+
     def reply(self):
         km = getUtility(IKeywordManager)
         query = {"withLengths": True}
         if idx := self.request.form.get("idx"):
             query["indexName"] = idx
 
-        keywords = km.getKeywords(**query)
+        if len(self.params) == 0:
+            keywords = km.getKeywords(**query)
+        else:
+            keywords = km.getKeyword(self.params[0])
 
         batch = HypermediaBatch(self.request, keywords)
-        items = [{"name": name, "total": count} for name, count in batch]
+        if len(self.params) == 0:
+            items = [{"name": name, "total": count} for name, count in batch]
+        else:
+            items = list(batch)
 
         keywords_data = {
             "@id": batch.canonical_url,
