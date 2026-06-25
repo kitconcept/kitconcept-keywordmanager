1793f793a48c6e8f9091d8d6941ec8e653f5c6d0
jnptk <110389276+jnptk@users.noreply.github.com>
2026-06-25 11:59:03 +0200

    more specific texts for rename modal if only one keyword is selected

--- frontend/packages/volto-keywordmanager/src/components/RenameModal.tsx
+++ frontend/packages/volto-keywordmanager/src/components/RenameModal.tsx
@@ -7,17 +7,19 @@ import {
   Select,
 } from '@plone/components';
 import Icon from '@plone/volto/components/theme/Icon/Icon';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import clearSVG from '@plone/volto/icons/clear.svg';
 import { Dialog } from '@plone/components';
 import { Spinner } from '@plone/components';
 import { useIntl, defineMessages } from 'react-intl';
 
+import type { Selection } from 'react-aria-components';
+
 interface RenameModalProps {
   isLoading: boolean;
   selectionCount: number;
-  selectedKeys: string | Set<string>;
+  selectedKeys: Selection;
   keywords: { items?: { name: string }[] };
   onConfirm: (newKeyword: string, oldKeywords: string[]) => void;
 }
@@ -26,6 +28,22 @@ const messages = defineMessages({
   loading: {
     id: 'loading',
     defaultMessage: 'Loading',
+  },
+  selectLabel: {
+    id: 'Select existing keyword to keep:',
+    defaultMessage: 'Select existing keyword to keep:',
+  },
+  selectPlaceholder: {
+    id: 'Please select ...',
+    defaultMessage: 'Please select ...',
+  },
+  textFieldLabel: {
+    id: 'New keyword name:',
+    defaultMessage: 'New keyword name:',
+  },
+  textFieldPlaceholder: {
+    id: 'Please enter new name',
+    defaultMessage: 'Please enter new name',
   },
 });
 
@@ -40,6 +58,14 @@ const RenameModal = ({
   const [selectedRadio, setSelectedRadio] = useState<string>('select');
   const [name, setName] = useState<string | null>(null);
 
+  useEffect(() => {
+    if (selectionCount === 1) {
+      setSelectedRadio('text');
+    } else {
+      setSelectedRadio('select');
+    }
+  }, [selectionCount]);
+
   return (
     <Modal className="rename-modal">
       <Dialog>
@@ -47,10 +73,17 @@ const RenameModal = ({
           <>
             <div className="modal-header">
               <h2>
-                <FormattedMessage
-                  id="rename-modal-title"
-                  defaultMessage="Rename & merge keyword(s)"
-                />
+                {selectionCount > 1 ? (
+                  <FormattedMessage
+                    id="rename-modal-title"
+                    defaultMessage="Rename & merge keyword(s)"
+                  />
+                ) : (
+                  <FormattedMessage
+                    id="single-rename-modal-title"
+                    defaultMessage="Rename keyword"
+                  />
+                )}
               </h2>
               <Button
                 onPress={() => {
@@ -63,44 +96,58 @@ const RenameModal = ({
             </div>
             <div className="modal-body">
               <p>
-                <FormattedMessage
-                  id="rename-modal-description"
-                  defaultMessage="You are about to rename {num} selected keyword(s). This action cannot be undone. Either select one of the existing keywords to keep or enter a new name to replace all selected keywords."
-                  values={{ num: <strong>{selectionCount}</strong> }}
-                />
+                {selectionCount > 1 ? (
+                  <FormattedMessage
+                    id="rename-modal-description"
+                    defaultMessage="You are about to rename & merge {num} selected keyword(s). This action cannot be undone. Either select one of the existing keywords to keep or enter a new name to replace all selected keywords."
+                    values={{ num: <strong>{selectionCount}</strong> }}
+                  />
+                ) : (
+                  <FormattedMessage
+                    id="single-rename-modal-description"
+                    defaultMessage="You are about to rename the keyword {keyword}. This action cannot be undone. Please enter a new name to proceed."
+                    values={{ keyword: <strong>{selectedKeys}</strong> }}
+                  />
+                )}
               </p>
               <RadioGroup
                 defaultValue="select"
                 value={selectedRadio}
                 onChange={setSelectedRadio}
               >
-                <div className="react-aria-Radio-wrapper">
-                  <Radio value="select" />
-                  <Select
-                    isDisabled={selectedRadio !== 'select'}
-                    label="Select existing keyword to keep:"
-                    selectionMode="single"
-                    placeholder="Please select ..."
-                    onChange={setName}
-                    items={
-                      selectedKeys === 'all'
-                        ? keywords.items?.map((kw) => ({
-                            label: kw.name,
-                            value: kw.name,
-                          }))
-                        : [...(selectedKeys as Set<string>)].map((kw) => ({
-                            label: kw,
-                            value: kw,
-                          }))
-                    }
-                  />
-                </div>
+                {selectionCount > 1 && (
+                  <div className="react-aria-Radio-wrapper">
+                    <Radio value="select" />
+                    <Select
+                      isDisabled={selectedRadio !== 'select'}
+                      label={intl.formatMessage(messages.selectLabel)}
+                      selectionMode="single"
+                      placeholder={intl.formatMessage(
+                        messages.selectPlaceholder,
+                      )}
+                      onChange={setName}
+                      items={
+                        selectedKeys === 'all'
+                          ? keywords.items?.map((kw) => ({
+                              label: kw.name,
+                              value: kw.name,
+                            }))
+                          : [...(selectedKeys as Set<string>)].map((kw) => ({
+                              label: kw,
+                              value: kw,
+                            }))
+                      }
+                    />
+                  </div>
+                )}
                 <div className="react-aria-Radio-wrapper">
                   <Radio value="text" />
                   <TextField
                     isDisabled={selectedRadio !== 'text'}
-                    label="New keyword name:"
-                    placeholder="Please enter new name"
+                    label={intl.formatMessage(messages.textFieldLabel)}
+                    placeholder={intl.formatMessage(
+                      messages.textFieldPlaceholder,
+                    )}
                     onChange={setName}
                   />
                 </div>
@@ -132,11 +179,13 @@ const RenameModal = ({
               >
                 {isLoading ? (
                   <Spinner aria-label={intl.formatMessage(messages.loading)} />
-                ) : (
+                ) : selectionCount > 1 ? (
                   <FormattedMessage
                     id="Rename & Merge"
                     defaultMessage="Rename & Merge"
                   />
+                ) : (
+                  <FormattedMessage id="Rename" defaultMessage="Rename" />
                 )}
               </Button>
             </div>
