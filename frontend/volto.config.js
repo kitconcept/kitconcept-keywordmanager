5c8389d2df9b98ff6ae4be6e753e2298007188e7
jnptk <110389276+jnptk@users.noreply.github.com>
2026-06-29 10:27:01 +0200

    add VLT as dev dependency

--- frontend/volto.config.js
+++ frontend/volto.config.js
@@ -1,5 +1,5 @@
-const addons = ["volto-keywordmanager"];
-const theme = "";
+const addons = ['volto-keywordmanager', '@kitconcept/volto-light-theme'];
+const theme = '@kitconcept/volto-light-theme';
 
 module.exports = {
   addons,
